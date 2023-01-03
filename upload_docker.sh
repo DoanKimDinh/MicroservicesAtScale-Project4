cbb16efe822422d86a8c1d393e6eb1d19614af31
DoanKimDinh <doankimdinh113@gmail.com>
2023-01-03 22:58:15 +0700

    add script

---
+++ upload_docker.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+dockerpath=doankimdinh/udacity_microservice:v1
+
+# Step 2:
+# Authenticate & tag
+export DOCKER_ID="doankimdinh"
+echo "Docker ID and Image: $dockerpath"
+docker tag doankimdinh/udacity_microservice:v1 $DOCKER_ID/udacity_microservice:v1
+
+# Step 3:
+# Push image to a docker repository
+docker login
+docker push $DOCKER_ID/udacity_microservice:v1
